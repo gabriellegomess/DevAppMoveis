ef47188b47d02cc606ae5e07b63768487d5b8123
gabriellegomess <128157932+gabriellegomess@users.noreply.github.com>
2023-08-10 10:32:54 -0300

    Comentários

--- Circunferencia/app/src/main/java/com/example/circunferencia/MainActivity.kt
+++ Circunferencia/app/src/main/java/com/example/circunferencia/MainActivity.kt
@@ -7,23 +7,34 @@ import android.widget.EditText
 import android.widget.TextView
 import kotlin.math.PI
 
+
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //Criando as variaveis que irão receber os valores para calcular
+
+        //variavel para o usuario inserir o valor do raio para que seja calculado a circunferência
         val radiusEditText: EditText = findViewById(R.id.radiusEditText)
+        //Botão para calcular, iniciar a aplicação do calculo
         val calculateButton: Button = findViewById(R.id.calculateButton)
+        //Resultado printado na tela
         val resultTextView: TextView = findViewById(R.id.resultTextView)
 
-        calculateButton.setOnClickListener {
+        //função que o botão calcular vai fazer
+        calculateButton.setOnClickListener {//vai realizar a função assim que clicarmos
+            //recebe a variavel do  raio
             val radiusText = radiusEditText.text.toString()
+            //Se o valor nao estiver vazio ele vai calcular a circunferência
             if (radiusText.isNotEmpty()) {
                 val radius = radiusText.toDouble()
-                val circumference = 2 * PI * radius
-                resultTextView.text = "Circunferência: $circumference"
+                val circumference = 2 * PI * radius //formula da circunferencia com o raio
+                //resultTextView.text = "Circunferência: $circumference" //printa o resultado
+                resultTextView.text = String.format("Área do Triângulo: %.2f", circumference)
             } else {
-                resultTextView.text = "Insira um raio válido"
+                resultTextView.text = "Insira um raio válido" //se raio estiver vazio ou invalido vai pedir um novo valor
             }
         }
     }
--- android/app/src/main/java/com/example/aula0908/MainActivity.kt
+++ android/app/src/main/java/com/example/aula0908/MainActivity.kt
@@ -15,22 +15,27 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //atribuindo as variaveis e os ID para realizar as funções
         baseEditText = findViewById(R.id.baseEditText)
         alturaEditText = findViewById(R.id.alturaEditText)
         resultadoTextView = findViewById(R.id.resultadoTextView)
     }
 
+    //função para calcular a area do triangulo baseado na base e altura
     fun calcularArea(view: View) {
+        //variavel para calcular, pegando o Id de cada uma delas
         val baseStr = baseEditText.text.toString()
         val alturaStr = alturaEditText.text.toString()
 
+        //condição para que se a base e a altura nao estiverem vazias a aplicação será realizada
         if (baseStr.isNotEmpty() && alturaStr.isNotEmpty()) {
             val base = baseStr.toDouble()
             val altura = alturaStr.toDouble()
-
+//formula para calcular, pegando a base a altura dadas pelo usuario
             val area = (base * altura) / 2
             resultadoTextView.text = String.format("Área do Triângulo: %.2f", area)
         } else {
+            //caso os valores estejam vazios
             resultadoTextView.text = "Insira valores válidos para base e altura."
         }
     }
